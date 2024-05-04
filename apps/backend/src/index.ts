a50baf75a4f44623c066da8cbc19d90253c3cfa7
brickly26 <sabburak26@gmail.com>
2024-05-04 14:27:16 -0400

    simple http server setup for auth

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,36 @@
 import express from "express";
+const cookieSession = require("cookie-session");
+const cors = require("cors");
+const passport = require("passprt");
 import dotenv from "dotenv";
 
 const app = express();
 
-dotenv;
+dotenv.config();
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["lama"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: "GET,POST,PUT,DELET",
+    credetials: true,
+  })
+);
+
+// app.use("/auth", authRoute);
+// app.use("/v1", v1Router);
+
+const PORT = process.env.PORT || 5173;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- apps/frontend/src/Components/ChessBoard.tsx
+++ apps/frontend/src/Components/ChessBoard.tsx
@@ -1,6 +1,7 @@
 import { Chess, Color, PieceSymbol, Square } from "chess.js";
 import { useState } from "react";
 import { MOVE } from "../Pages/Game";
+import MoveTable from "./MovesTable";
 
 interface ChessBoardProps {
   color: string | null;
@@ -23,81 +24,84 @@ interface ChessBoardProps {
 }
 
 const ChessBoard: React.FC<ChessBoardProps> = ({
-  color,
   board,
   socket,
   chess,
   setBoard,
 }) => {
   const [from, setFrom] = useState<null | Square>(null);
-  const [to, setTo] = useState<null | Square>(null);
+  const [moves, setMoves] = useState<{ from: Square; to: Square }[]>([]);
 
   return (
-    <div>
-      {board.map((row, i) => {
-        return (
-          <div key={i} className="flex">
-            {row.map((square, j) => {
-              const squareCoord = (String.fromCharCode(97 + (j % 8)) +
-                "" +
-                (8 - i)) as Square;
-
-              return (
-                <div
-                  key={j}
-                  className={`w-16 h-16 ${
-                    (i + j) % 2 === 0 ? "bg-[#F0D8B5]" : "bg-[#B48764]"
-                  }`}
-                  onClick={() => {
-                    if (!from) {
-                      setFrom(squareCoord);
-                    } else if (!to) {
-                      setTo(squareCoord);
-                      socket.send(
-                        JSON.stringify({
-                          type: MOVE,
-                          payload: {
-                            move: {
-                              from,
-                              to: squareCoord,
-                            },
-                          },
-                        })
-                      );
-
-                      chess.move({
-                        from,
-                        to: squareCoord,
-                      });
-                      setBoard(chess.board());
-                      setFrom(null);
-                      setTo(null);
-
-                      console.log({
-                        from,
-                        to: squareCoord,
-                      });
-                    }
-                  }}
-                >
-                  <div className="w-full h-full flex justify-center items-center">
-                    {square ? (
-                      <img
-                        className="w-10"
-                        src={`/${
-                          square?.color === "b"
-                            ? square?.type
-                            : `${square?.type?.toUpperCase()} copy`
-                        }.png`}
-                      />
-                    ) : null}
-                  </div>
-                </div>
-              );
-            })}
+    <div className="flex">
+      <div>
+        {moves.length > 0 && (
+          <div className="mt-4">
+            <MoveTable moves={moves} />
           </div>
-        );
-      })}
+        )}
+      </div>
+      <div>
+        {board.map((row, i) => {
+          return (
+            <div key={i} className="flex">
+              {row.map((square, j) => {
+                const squareCoord = (String.fromCharCode(97 + (j % 8)) +
+                  "" +
+                  (8 - i)) as Square;
+
+                return (
+                  <div
+                    key={j}
+                    className={`w-16 h-16 ${
+                      (i + j) % 2 === 0 ? "bg-[#F0D8B5]" : "bg-[#B48764]"
+                    }`}
+                    onClick={() => {
+                      if (!from) {
+                        setFrom(squareCoord);
+                      } else {
+                        socket.send(
+                          JSON.stringify({
+                            type: MOVE,
+                            payload: {
+                              move: {
+                                from,
+                                to: squareCoord,
+                              },
+                            },
+                          })
+                        );
+
+                        chess.move({
+                          from,
+                          to: squareCoord,
+                        });
+                        setBoard(chess.board());
+                        setFrom(null);
+
+                        setMoves([...moves, { from, to: squareCoord }]);
+                      }
+                    }}
+                  >
+                    <div className="w-full h-full flex justify-center items-center">
+                      {square ? (
+                        <img
+                          className="w-10"
+                          src={`/${
+                            square?.color === "b"
+                              ? square?.type
+                              : `${square?.type?.toUpperCase()} copy`
+                          }.png`}
+                        />
+                      ) : null}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
