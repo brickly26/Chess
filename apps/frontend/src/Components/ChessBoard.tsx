c2aeeb410eaa85e0b57de7823c93a896f341a6d2
brickly26 <sabburak26@gmail.com>
2024-05-05 17:38:35 -0400

    added drag and drop functionality and diplay possible moves

--- apps/frontend/src/Components/ChessBoard.tsx
+++ apps/frontend/src/Components/ChessBoard.tsx
@@ -30,6 +30,63 @@ const ChessBoard: React.FC<ChessBoardProps> = ({
 }) => {
   const [from, setFrom] = useState<null | Square>(null);
   const [moves, setMoves] = useState<{ from: Square; to: Square }[]>([]);
+  const [validSquares, setValidSquares] = useState<[number, number][]>([]);
+
+  const handleDragStart = (e: React.DragEvent, square: Square) => {
+    e.dataTransfer.setData("text/plain", square);
+    setFrom(square);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent, to: Square) => {
+    e.preventDefault();
+    if (from) {
+      socket.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: {
+            move: {
+              from,
+              to,
+            },
+          },
+        })
+      );
+
+      chess.move({
+        from,
+        to,
+      });
+      setBoard(chess.board());
+      setFrom(null);
+    }
+  };
+
+  const validMoves = (square: Square | null) => {
+    const moves = chess.moves({ square: square! });
+
+    const algebraicToIndices = (square: string): [number, number] => {
+      let file: number, rank: number;
+
+      if (square.length === 2) {
+        file = square.charCodeAt(0) - "a".charCodeAt(0);
+        rank = 8 - parseInt(square.substring(1));
+      } else if (square.length === 3) {
+        file = square.charCodeAt(1) - "a".charCodeAt(0);
+        rank = 8 - parseInt(square.substring(2));
+      } else {
+        throw new Error("Invalid square notation");
+      }
+
+      return [rank, file];
+    };
+
+    const moveIndices = moves.map((move) => algebraicToIndices(move));
+    setValidSquares(moveIndices);
+  };
 
   return (
     <div className="flex">
@@ -55,44 +112,32 @@ const ChessBoard: React.FC<ChessBoardProps> = ({
                     className={`w-16 h-16 ${
                       (i + j) % 2 === 0 ? "bg-[#F0D8B5]" : "bg-[#B48764]"
                     }`}
-                    onClick={() => {
-                      if (!from) {
-                        setFrom(squareCoord);
-                      } else {
-                        socket.send(
-                          JSON.stringify({
-                            type: MOVE,
-                            payload: {
-                              move: {
-                                from,
-                                to: squareCoord,
-                              },
-                            },
-                          })
-                        );
-
-                        chess.move({
-                          from,
-                          to: squareCoord,
-                        });
-                        setBoard(chess.board());
-                        setFrom(null);
-
-                        setMoves([...moves, { from, to: squareCoord }]);
-                      }
-                    }}
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, squareCoord)}
                   >
                     <div className="w-full h-full flex justify-center items-center">
-                      {square ? (
-                        <img
-                          className="w-10"
-                          src={`/${
-                            square?.color === "b"
-                              ? square?.type
-                              : `${square?.type?.toUpperCase()} copy`
-                          }.png`}
-                        />
+                      {validSquares.length > 0 ? (
+                        <div
+                          className={`${validSquares.some((square) => square[0] === i && square[1] === j) ? "bg-yellow-200 absolute p-2 flex justify-center items-center h-2 w-2 rounded-[50%]" : ""}`}
+                        ></div>
                       ) : null}
+                      <div
+                        onClick={() => validMoves(square && square.square)}
+                        className="h-full justify-center flex flex-col"
+                      >
+                        {square ? (
+                          <img
+                            className="w-10"
+                            src={`/${
+                              square?.color === "b"
+                                ? square?.type
+                                : `${square?.type?.toUpperCase()} copy`
+                            }.png`}
+                            draggable={true}
+                            onDragStart={(e) => handleDragStart(e, squareCoord)}
+                          />
+                        ) : null}
+                      </div>
                     </div>
                   </div>
                 );
